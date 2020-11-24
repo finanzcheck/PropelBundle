1b43bdb6e45d72ebec90ea178cb0eee76d9c474f
Micha Waterböhr <micha.waterboehr@gmail.com>
2020-11-24 17:48:43 +0100

    Enable setting output-dir option for build model command

--- Command/ModelBuildCommand.php
+++ Command/ModelBuildCommand.php
@@ -34,6 +34,7 @@ class ModelBuildCommand extends WrappedCommand
 
             ->addOption('connection', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Connection to use. Example: default, bookstore')
             ->addOption('schema-dir', null, InputOption::VALUE_REQUIRED, 'The directory where the schema files are placed')
+            ->addOption('output-dir', null, InputOption::VALUE_REQUIRED, 'The output directory')
             ->addArgument('bundle', InputArgument::OPTIONAL, 'The bundle to generate model classes from')
         ;
     }
@@ -51,7 +52,8 @@ class ModelBuildCommand extends WrappedCommand
      */
     protected function getSubCommandArguments(InputInterface $input)
     {
-        $outputDir = $this->getApplication()->getKernel()->getRootDir().'/../';
+        $outputDirDefault = $this->getApplication()->getKernel()->getRootDir().'/../';
+        $outputDir = $this->input->getOption('output-dir') ?: $outputDirDefault;
 
         return array(
             '--output-dir' => $outputDir,
